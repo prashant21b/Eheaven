c4740a77da01d576b9b96a1c02929fde5f1743b1
Prashant kumar jha <113293467+prashant21b@users.noreply.github.com>
2024-07-03 13:08:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,11 +13,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000"); 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  })
+// app.use((req,res,next)=>{
+//     res.setHeader("Access-Control-Allow-Origin","http://localhost:3000"); 
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   })
 
 app.get('/',(req,res)=>{
     res.send("hello");
